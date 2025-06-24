3ac8018698d022e4fd6e67be1001d1769d03d316
Kariuki Karanja <kariukimarkbrian@gmail.com>
2025-06-24 11:40:05 +0300

    :lipstick: improve loading spiner

--- src/Resources/LoadingSpinner.jsx
+++ src/Resources/LoadingSpinner.jsx
@@ -4,7 +4,14 @@ import Styles from "../Assets/Styles/LoadingSpinner.module.css";
 export default function LoadingSpinner() {
   return (
     <div className={Styles.loadingSpinner}>
-      <div className={Styles.spinner}></div>
+      <div className={Styles.dotSpinner}>
+        <div className={Styles.dot}></div>
+        <div className={Styles.dot}></div>
+        <div className={Styles.dot}></div>
+        <div className={Styles.dot}></div>
+        <div className={Styles.dot}></div>
+        <div className={Styles.dot}></div>
+      </div>
     </div>
   );
 }
